5c03226858bcb9a64e1e86681aa679c20517904f
Dainen Dunn <lild31415@gmail.com>
2022-04-12 00:32:33 -0500

    Add project files.

---
+++ EMF_to_SVG/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace EMF_to_SVG
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openEmfBtn = new System.Windows.Forms.Button();
+            this.selectedEMFBox = new System.Windows.Forms.TextBox();
+            this.saveSVGBtn = new System.Windows.Forms.Button();
+            this.outputSVGFileBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // openEmfBtn
+            // 
+            this.openEmfBtn.Location = new System.Drawing.Point(12, 12);
+            this.openEmfBtn.Name = "openEmfBtn";
+            this.openEmfBtn.Size = new System.Drawing.Size(75, 23);
+            this.openEmfBtn.TabIndex = 0;
+            this.openEmfBtn.Text = "Select EMF";
+            this.openEmfBtn.UseVisualStyleBackColor = true;
+            this.openEmfBtn.Click += new System.EventHandler(this.openEmfBtn_Click);
+            // 
+            // selectedEMFBox
+            // 
+            this.selectedEMFBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.selectedEMFBox.Location = new System.Drawing.Point(93, 15);
+            this.selectedEMFBox.Name = "selectedEMFBox";
+            this.selectedEMFBox.ReadOnly = true;
+            this.selectedEMFBox.Size = new System.Drawing.Size(310, 20);
+            this.selectedEMFBox.TabIndex = 1;
+            // 
+            // saveSVGBtn
+            // 
+            this.saveSVGBtn.Location = new System.Drawing.Point(12, 41);
+            this.saveSVGBtn.Name = "saveSVGBtn";
+            this.saveSVGBtn.Size = new System.Drawing.Size(75, 23);
+            this.saveSVGBtn.TabIndex = 2;
+            this.saveSVGBtn.Text = "Save SVG";
+            this.saveSVGBtn.UseVisualStyleBackColor = true;
+            this.saveSVGBtn.Click += new System.EventHandler(this.saveSVGBtn_Click);
+            // 
+            // outputSVGFileBox
+            // 
+            this.outputSVGFileBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.outputSVGFileBox.Location = new System.Drawing.Point(93, 41);
+            this.outputSVGFileBox.Name = "outputSVGFileBox";
+            this.outputSVGFileBox.ReadOnly = true;
+            this.outputSVGFileBox.Size = new System.Drawing.Size(310, 20);
+            this.outputSVGFileBox.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(439, 78);
+            this.Controls.Add(this.outputSVGFileBox);
+            this.Controls.Add(this.saveSVGBtn);
+            this.Controls.Add(this.selectedEMFBox);
+            this.Controls.Add(this.openEmfBtn);
+            this.Name = "Form1";
+            this.Text = "EMF to SVG";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openEmfBtn;
+        private System.Windows.Forms.TextBox selectedEMFBox;
+        private System.Windows.Forms.Button saveSVGBtn;
+        private System.Windows.Forms.TextBox outputSVGFileBox;
+    }
+}
+
---
+++ EMF_to_SVG/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Aspose.Imaging;
+using Aspose.Imaging.ImageOptions;
+using Color = Aspose.Imaging.Color;
+using Image = Aspose.Imaging.Image;
+
+namespace EMF_to_SVG
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openEmfBtn_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "EMF files (*.emf)|*.emf";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+                openFileDialog.Multiselect = false;
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    this.selectedEMFBox.Text = openFileDialog.FileName;
+                }
+            }
+        }
+
+        private void saveSVGBtn_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.InitialDirectory = "c:\\";
+                saveFileDialog.Filter = "SVG files (*.svg)|*.svg";
+                saveFileDialog.FilterIndex = 2;
+                saveFileDialog.RestoreDirectory = true;
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    this.outputSVGFileBox.Text = saveFileDialog.FileName;
+
+                    EMF_SVG(this.selectedEMFBox.Text, this.outputSVGFileBox.Text);
+                }
+            }
+        }
+
+        private void EMF_SVG(string inEMF, string outSVG)
+        {
+
+            //// Path to input EMF file
+            //string sourcePath = dataDir + @"test.emf";
+
+            //// Path for output SVG image
+            //string destPath = dataDir + @"EMF_out.svg";
+
+            // Load input EMF file
+            using (Image image = Image.Load(inEMF))
+            {
+                // Initilaize EmfRasterizationOptions class object
+                EmfRasterizationOptions emfRasterizationOptions = new EmfRasterizationOptions();
+
+                emfRasterizationOptions.BackgroundColor = image.BackgroundColor;
+                emfRasterizationOptions.PageWidth = image.Width;
+                emfRasterizationOptions.PageHeight = image.Height;
+
+                // Save output SVG image
+                image.Save(outSVG, new SvgOptions() { VectorRasterizationOptions = emfRasterizationOptions });
+            }
+        }
+    }
+}
